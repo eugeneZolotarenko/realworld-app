5b70f17266d54d28e9b7230ea48372b6e3dea504
eugeneZolotarenko <zolotarenko.yevheniy1998@gmail.com>
2020-05-24 18:09:17 +0200

    Adding hard code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react"
 import { Route, Switch } from "react-router-dom"
 
-import { Helmet } from "react-helmet"
-
 import Home from "./pages/Home"
 import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
@@ -18,21 +16,6 @@ import Footer from "./components/Footer"
 function App() {
   return (
     <div className='App'>
-      <Helmet>
-        <meta charset='utf-8' />
-        <title>Conduit</title>
-        <link
-          href='//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css'
-          rel='stylesheet'
-          type='text/css'
-        />
-        <link
-          href='//fonts.googleapis.com/css?family=Titillium+Web:700|Source+Serif+Pro:400,700|Merriweather+Sans:400,700|Source+Sans+Pro:400,300,600,700,300italic,400italic,600italic,700italic'
-          rel='stylesheet'
-          type='text/css'
-        />
-        <link rel='stylesheet' href='//demo.productionready.io/main.css' />
-      </Helmet>
       <Header />
       <Switch>
         <Route exact path='/' component={Home} />
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import App from "./App"
-import store from "./store"
+
 import { Provider } from "react-redux"
 import * as serviceWorker from "./serviceWorker"
-
 import { BrowserRouter } from "react-router-dom"
+
+import App from "./App"
+import store from "./store"
 
 ReactDOM.render(
   <React.StrictMode>
