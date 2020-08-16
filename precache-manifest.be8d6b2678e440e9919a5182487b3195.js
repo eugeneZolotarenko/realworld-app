9206dc6a4e93ae5395a38890df2bde78db21e197
eugeneZolotarenko <zolotarenko.yevheniy1998@gmail.com>
2020-08-16 09:22:44 +0200

    Updates

---
+++ precache-manifest.be8d6b2678e440e9919a5182487b3195.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e10f0391fc5953c851aa363be2b6c3d2",
+    "url": "/realworld-app/index.html"
+  },
+  {
+    "revision": "0b66cab78e09ac04cb37",
+    "url": "/realworld-app/static/js/2.24e30955.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/realworld-app/static/js/2.24e30955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e3662ab01761aa6a9c2",
+    "url": "/realworld-app/static/js/main.1276e158.chunk.js"
+  },
+  {
+    "revision": "0f56cbd9eacde5e8a523",
+    "url": "/realworld-app/static/js/runtime-main.a85487f5.js"
+  }
+]);
